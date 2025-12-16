13c81340fe4ca6048efaf1517f5a47718c7dcdcb
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-12-16 13:36:35 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function calculateSquare(){
     let value = document.getElementById('number').value;
     let result = document.getElementById('result');
-
+document.getElementById('result').innerHTML = value*value;
+if (value != number) {
+    document.getElementById('result').innerHTML = 'Invalid, please enter a number';
+}
     //add code here
     
 }
